eae806cc4519a188ae7ebf53358902e498d6b911
luddekn <70239345+luddekn@users.noreply.github.com>
2024-12-06 22:11:18 +0100

    Removed polybar and rofi

--- polybar/.config/polybar/launch_polybar.sh
+++
@@ -1,8 +0,0 @@
-if type "xrandr"; then
-  polybar --reload --config=/home/lk/.config/polybar/config.ini toph 
- # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
- #   MONITOR=$m polybar --reload --config=/home/lk/.config/polybar/config.ini toph &
- # done
-else	
-  polybar --reload --config=/home/lk/.config/polybar/config.ini toph 
-fi
