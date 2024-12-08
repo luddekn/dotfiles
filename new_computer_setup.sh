d0e8a4776e137882f8a4bda7566138da47680d55
Ludvik Kristoffersen <70239345+luddekn@users.noreply.github.com>
2024-12-08 11:15:18 +0100

    Update and rename setupnewcomputer.sh to new_computer_setup.sh

--- new_computer_setup.sh
+++ new_computer_setup.sh
@@ -5,8 +5,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo
 
 echo "Installing core stuff"
-echo -e "\txorg\n\ti3\n\tlightdm\n\talacritty\n\tvim\n\tnetwork-manager\n\tgh\n\tzsh & zsh-syntax-highlighting\n\tpicom\n\tstow\n\tpulseaudio & pavucontrol\n\tblueman & libspa-0.2-bluetooth\n\tzip\n\tgpg\n\tufw\n\tthunar"
-sudo apt-get install -y xorg i3 lightdm alacritty vim network-manager gh zsh zsh-syntax-highlighting picom stow pulseaudio pavucontrol blueman libspa-0.2-bluetooth zip gpg ufw thunar
+echo -e "\txorg\n\ti3\n\tlightdm\n\tcurl\n\tvim\n\tnetwork-manager\n\tgh\n\tzsh & zsh-syntax-highlighting\n\tpicom\n\tstow\n\tpulseaudio & pavucontrol\n\tblueman & libspa-0.2-bluetooth\n\tzip\n\tgpg\n\tufw\n\tthunar"
+sudo apt-get install -y xorg i3 lightdm curl vim network-manager gh zsh zsh-syntax-highlighting picom stow pulseaudio pavucontrol blueman libspa-0.2-bluetooth zip gpg ufw thunar
 echo
 
 echo "Enabling lightdm"
@@ -31,6 +31,9 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 echo
 
+echo "Do you want to install Brave and Spotify? (y/n)"
+read -r choice
+
 echo "Adding Brave browser to apt source list, then downloading"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg || { echo "Brave key download failed"; exit 1; }
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -44,3 +47,8 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install spotify-client -y
 echo
+
+echo "Removing desktop-base for the removal of default Debian background in grub and i3"
+sudo apt-get purge desktop-base -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
