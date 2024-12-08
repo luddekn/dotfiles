f6684d3fe4e4c14a06311b44c7ffe4853d5358e4
luddekn <70239345+luddekn@users.noreply.github.com>
2024-12-08 17:40:09 +0100

    Changes to monitor-setup.sh script, changed 140 to 144

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xrandr --output DP-2 --mode 1920x1080 --rate 240 --primary
-xrandr --output HDMI-1 --mode 1920x1080 --rate 140 --left-of DP-2
+xrandr --output HDMI-1 --mode 1920x1080 --rate 144 --left-of DP-2
