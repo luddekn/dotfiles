ede8071c074d69abc2576e23b11e50d45d424f92
luddekn <70239345+luddekn@users.noreply.github.com>
2024-12-11 03:02:33 +0100

    Added more stuff in set.lua, also added a new keymap for deleting search in normal mode

--- nvim/.config/nvim/lua/ludde/remap.lua
+++ nvim/.config/nvim/lua/ludde/remap.lua
@@ -16,3 +16,13 @@ vim.keymap.set("n", "<leader>s", ":Neotree filesystem reveal left<CR>")
 
 -- Escape insert mode by pressing "jj"
 vim.keymap.set("i", "jj", "<Esc>")
+
+-- Clears the search filter when in normal mode
+vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>")
+
+-- Disabling arrow keys in normal mode
+vim.keymap.set("n", "<left>", "<cmd>echo 'Use h to move left'<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo 'Use l to move right'<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo 'Use k to move up'<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo 'Use j to move down'<CR>")
+
--- nvim/.config/nvim/lua/ludde/set.lua
+++ nvim/.config/nvim/lua/ludde/set.lua
@@ -12,3 +12,31 @@ vim.opt.wrap = false
 vim.opt.guicursor = ""
 
 vim.opt.termguicolors = true
+
+vim.opt.breakindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.scrolloff = 10
+
+-- Makes nvim and OS share clipboard
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.opt.hlsearch = true
+
+-- Quickly highlights the text you are copying
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
