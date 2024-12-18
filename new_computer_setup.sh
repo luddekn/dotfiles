581d009224672cf34da9fdd87f9f67d89babedae
luddekn <70239345+luddekn@users.noreply.github.com>
2024-12-18 11:13:46 +0100

    Added alacritty and nvim installers in the new_computer_setup.sh script. Also removed picom from the list of things to install

--- new_computer_setup.sh
+++ new_computer_setup.sh
@@ -18,17 +18,17 @@ echo
 # Enabling lightdm on boot
 echo "Enabling lightdm"
 if ! systemctl is-enabled --quiet lightdm; then
-  sudo systemctl enable lightdm
+    sudo systemctl enable lightdm
 else
-  echo "lightdm is already enabled."
+    echo "lightdm is already enabled."
 fi
 echo
 # Enabling networkmanager on boot
 echo "Enabling NetworkManager"
 if ! systemctl is-enabled --quiet NetworkManager; then
-  sudo systemctl enable NetworkManager
+    sudo systemctl enable NetworkManager
 else
-  echo "NetworkManager is already enabled."
+    echo "NetworkManager is already enabled."
 fi
 echo
 # Enabling firewall on boot and setting rules: DENY ALL inc - ALLOW ALL out
@@ -38,26 +38,66 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 echo
 
+# Installing alacritty terminal
+echo "Do you want to install alacritty? (y/n)"
+read -r alacritty
+
+if [[ "$alacritty" == "y" || "$alacritty" == "Y" ]]; then
+    # Installing dependencies
+    sudo apt install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+    cd
+    # Installing rustup
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # Cloning alacritty git repository
+    git clone https://github.com/alacritty/alacritty.git
+    ~/.cargo/bin/rustup override set stable
+    ~/.cargo/bin/rustup update stable
+    # Building alacritty
+    cd alacritty
+    ~/.cargo/bin/cargo build --release --no-default-features --features=x11
+else
+    echo "Not installing alacritty"
+fi
+
+# Installing nvim editor
+echo "Do you want to install nvim? (y/n)"
+read -r nvim
+
+if [[ "$nvim" == "y" || "$nvim" == "Y" ]]; then
+    cd /opt
+    sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo tar -xzf nvim-linux64.tar.gz
+    sudo rm -rf nvim-linux64.tar.gz
+else
+    echo "Not installing nvim"
+fi
+
 # Asking user if they want to install Brave browser and Spotify now, could always install later!
 echo "Do you want to install Brave and Spotify? (y/n)"
 read -r choice
 
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-	echo "Adding Brave browser to apt source list, then downloading"
-	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg || { echo "Brave key download failed"; exit 1; }
-	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-	sudo apt-get update
-	sudo apt-get install brave-browser -y
-	echo
+    echo "Adding Brave browser to apt source list, then downloading"
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg || {
+        echo "Brave key download failed"
+        exit 1
+    }
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt-get update
+    sudo apt-get install brave-browser -y
+    echo
 
-	echo "Adding Spotify to apt source list, then downloading"
-	curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg || { echo "Spotify key download failed"; exit 1; }
-	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt-get update
-	sudo apt-get install spotify-client -y
-	echo
+    echo "Adding Spotify to apt source list, then downloading"
+    curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg || {
+        echo "Spotify key download failed"
+        exit 1
+    }
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update
+    sudo apt-get install spotify-client -y
+    echo
 else
-	echo "Not installing Brave or Spotify"
+    echo "Not installing Brave or Spotify"
 fi
 
 # Removing the desktop-base package to remove the Debian wallpaper on GRUB and background
