1b0a1ebfb8f467803940c89f9624c48fd120b29d
luddekn <70239345+luddekn@users.noreply.github.com>
2025-09-03 11:07:19 +0200

    i3 color changes, nvim changes

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,35 +1,38 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" },
-        cmd = { "ConformInfo" },
-        keys = {
-            {
-                "<leader>f",
-                function()
-                    require("conform").format({ async = true })
-                end,
-                mode = "",
-                desc = "Format buffer",
-            },
-        },
-        opts = {
-            -- Define your formatters
-            formatters_by_ft = {
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                html = { "prettier" },
-                css = { "prettier" },
-                scss = { "prettier" },
-                bash = { "shfmt" },
-                go = { "gofmt", "goimports" },
-            },
-            -- Set default options
-            default_format_opts = {
-                lsp_format = "fallback",
-            },
-            -- Set up format-on-save
-            format_on_save = { timeout_ms = 500 },
-        },
-    },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd" },
+				html = { "prettier" },
+				bash = { "shfmt" },
+				go = { "gofmt", "goimports" },
+			},
+			formatters = {
+				prettier = {
+					prepend_args = { "--parser", "html" },
+				},
+			},
+			-- Set default options
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500 },
+		},
+	},
 }
