b6ae28b20fac639c283e57c87166f4fb74280c75
luddekn <70239345+luddekn@users.noreply.github.com>
2025-09-06 01:32:30 +0200

    Added back the solarized-osaka theme, made it auto update lualine depending on the current theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,47 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-        require('rose-pine').setup({
-            disable_background = true,
-            styles = {
-                italic = false,
-            },
-        })
-
-        ColorMyPencils();
-    end
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		config = function()
+			local name = "solarized-osaka"
+			require("solarized-osaka").setup({
+				disable_background = true,
+				styles = {
+					floats = "dark",
+					sidebar = "dark",
+					italic = false,
+				},
+				hide_inactive_statusline = true,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			ColorMyPencils(name)
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					floats = "dark",
+					sidebar = "dark",
+					italic = false,
+				},
+				hide_inactive_statusline = true,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			ColorMyPencils()
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,18 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
+		local theme = vim.cmd.colorscheme()
+		local lualineTheme
+		if theme == "rose-pine" then
+			lualineTheme = "rose-pine"
+		elseif theme == "solarized-osaka" then
+			lualineTheme = "solarized_dark"
+		else
+			lualineTheme = "auto"
+		end
 		require("lualine").setup({
 			options = {
-				theme = "rose-pine",
+				theme = lualineTheme,
 			},
 		})
 	end,
