6c6e61f23f10de6dca08a1c31a8df694e420ddb5
luddekn <70239345+luddekn@users.noreply.github.com>
2025-09-06 15:22:08 +0200

    Small adjustment to changeColor() function

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,53 +1,53 @@
 function changeColor(id)
-    local colorScheme
-    if id == 1 then
-        colorScheme = "rose-pine"
-    elseif id == 2 then
-        colorScheme = "solarized-osaka"
-    else
-        colorScheme = "rose-pine"
-    end
-    vim.cmd.colorscheme(colorScheme)
+	local colorScheme
+	if id == 1 then
+		colorScheme = "rose-pine"
+	elseif id == 2 then
+		colorScheme = "solarized-osaka"
+	else
+		colorScheme = "solarized-osaka"
+	end
+	vim.cmd.colorscheme(colorScheme)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    {
-        "craftzdog/solarized-osaka.nvim",
-        lazy = false,
-        config = function()
-            require("solarized-osaka").setup({
-                disable_background = true,
-                styles = {
-                    floats = "dark",
-                    sidebar = "dark",
-                    italic = false,
-                },
-                hide_inactive_statusline = true,
-                dim_inactive = true,
-                lualine_bold = true,
-            })
-            changeColor(2)
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                styles = {
-                    floats = "dark",
-                    sidebar = "dark",
-                    italic = false,
-                },
-                hide_inactive_statusline = true,
-                dim_inactive = true,
-                lualine_bold = true,
-            })
-            changeColor(1)
-        end,
-    },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		config = function()
+			require("solarized-osaka").setup({
+				disable_background = true,
+				styles = {
+					floats = "dark",
+					sidebar = "dark",
+					italic = false,
+				},
+				hide_inactive_statusline = true,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			changeColor(2)
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					floats = "dark",
+					sidebar = "dark",
+					italic = false,
+				},
+				hide_inactive_statusline = true,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			changeColor(1)
+		end,
+	},
 }
