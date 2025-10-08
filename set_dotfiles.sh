8039fb52c6892813dafc3a13ecf7942e1e39ef8d
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-08 15:49:11 +0200

    Comment change

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -2,7 +2,6 @@
 exclude_dirs=("xorg" "scripts" "systemd")
 
 # For each file in the ~/dotfiles directory, use stow on all sub-directories excluding the directories
-# depending on the user choice on line 7
 for dir in *; do
     if [[ -d "$dir" ]]; then
         if [[ " ${exclude_dirs[*]} " == *" $dir "* ]]; then
