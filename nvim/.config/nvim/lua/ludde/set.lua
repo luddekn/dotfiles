749be4365ba3464490e55f9684eba5f6e118f9ee
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-15 00:09:56 +0200

    Added rounded borders around all floating windows

--- nvim/.config/nvim/lua/ludde/set.lua
+++ nvim/.config/nvim/lua/ludde/set.lua
@@ -29,15 +29,15 @@ vim.opt.updatetime = 50
 
 -- Quickly highlights the text you are copying
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup(
-        "kickstart-highlight-yank",
-        { clear = true }
-    ),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Open neotree when nvim starts
 vim.cmd([[autocmd VimEnter * Neotree show]])
+
+-- Setting borders for all floating windows to rounded
+vim.o.winborder = "rounded"
