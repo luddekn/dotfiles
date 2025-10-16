23333d0d66b9b83604023ba8dd931018c6fc6c97
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-16 18:38:19 +0200

    Set background to solid black

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 -- Plugin for nvim colorscheme, using solarized-osaka
 return {
 	"vague-theme/vague.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other plugins
+	lazy = false,
+	priority = 1000,
 	config = function()
-		-- NOTE: you do not need to call setup if you don't want to.
 		require("vague").setup({
-			-- optional configuration here
+			transparent = true,
 		})
 		vim.cmd("colorscheme vague")
 	end,
