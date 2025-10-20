b18e8a707d4d64b3a3d9dc751cb33cbc01cab7f7
luddekn <70239345+luddekn@users.noreply.github.com>
2025-10-20 13:12:30 +0200

    Changed background color for markdown H1 to vauge red

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -5,4 +5,14 @@ return {
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
 	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			heading = {
+				custom = {
+					{ pattern = "^# ", foreground = "HeadingH1", background = "HeadingH1", icon = "󰲡 " },
+				},
+			},
+		})
+		vim.api.nvim_set_hl(0, "HeadingH1", { fg = "#FFFFFF", bold = true, bg = "#5e2f38" })
+	end,
 }
