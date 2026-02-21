85af1237bcb883209ca554669259ee06df08db85
luddekn <70239345+luddekn@users.noreply.github.com>
2026-02-21 08:31:04 +0100

    i3 changes, change fan speed on GPU

--- scripts/gpu_start.sh
+++ scripts/gpu_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FAN_SPEED=60
+FAN_SPEED=70
 
 sudo nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$FAN_SPEED" -a "[fan:1]/GPUTargetFanSpeed=$FAN_SPEED"
