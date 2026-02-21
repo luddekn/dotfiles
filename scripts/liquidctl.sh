40378367103be18576766f7b58028cb4b93d4b3a
Ludvik <70239345+luddekn@users.noreply.github.com>
2026-02-21 08:38:15 +0100

    Update liquidctl.sh

--- scripts/liquidctl.sh
+++ scripts/liquidctl.sh
@@ -2,4 +2,4 @@
 
 liquidctl initialize all
 liquidctl --match kraken set pump speed 100
-liquidctl --match kraken set fan speed 25 50  30 60  35 70  40 80  45 90  50 100
+liquidctl --match kraken set fan speed 20 75  30 75  40 75  50 80  55 90  60 100
